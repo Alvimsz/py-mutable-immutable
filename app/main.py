2934f621154fe3baefc07c24da93ed6ddaa2b4ad
Miguel Alvim <99217271+Alvimsz@users.noreply.github.com>
2026-09-10 19:05:39 -0300

    Refactor sorted_variables with new data types

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
-a = 1
-b = 2.0
-c = "string"
-d = (1, 2, 3)
-e = None
-f = [1, 2, 3]
-g = {"key": "value"}
-h = {1, 2, 3}
+lucky_number = 777
+pi = 3.14
+one_is_a_prime_number = False
+name = "Richard"
+my_favourite_films = [
+    "The Shawshank Redemption",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "The Matrix",
+]
+profile_info = ("michel", "michel@gmail.com", "12345678")
+marks = {
+    "John": 4,
+    "Sergio": 3,
+}
+collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
-    "mutable": [f, g, h],
-    "immutable": [a, b, c, d, e]
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info],
 }
